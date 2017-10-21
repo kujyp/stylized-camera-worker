a5a48323ab868fe0f430a64f7877e44b18c1758b
pjo901018 <pjo901018@korea.ac.kr>
2017-10-21 18:56:50 +0900

    Fix bug on upload_to_storage

--- firebase/storage/uploader.py
+++ firebase/storage/uploader.py
@@ -10,11 +10,12 @@ def path_join_on_firebase_storage(path1, path2):
 def upload_to_storage(filename):
     storage = Firebase().get_storage()
     path = path_join_on_firebase_storage(config.STORAGE.DONE_PATH, filename)
+    import os
+    localpath = os.path.join(config.OUTPUT_DIR, filename)
     storage.child(path).put(filename)
     url = storage.child(path).get_url(False)
     print("upload_to_storage/url = %s" % url)
 
-    import os
-    remove_file(os.path.join(config.OUTPUT_DIR, filename))
+    remove_file(localpath)
 
     return url
